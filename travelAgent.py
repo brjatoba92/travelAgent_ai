9b1018b3c4c28ba4476d5001ce5f148c60b7821f
brjatoba92 <brjatoba92@gmail.com>
2024-07-10 22:34:56 -0300

    Agent Supervisor created

--- travelAgent.py
+++ travelAgent.py
@@ -7,21 +7,17 @@ from langchain import hub
 
 llm = ChatOpenAI(model="gpt-3.5-turbo")
 
-tools = load_tools(['ddg-search', 'wikipedia'], llm= llm)
-
-# print(tools[1].name, tools[1].description)
-
-prompt = hub.pull("hwchase17/react")
-
-agent = create_react_agent(llm, tools, prompt)
-
-agent_executor = AgentExecutor(agent=agent, tools=tools, prompt=prompt, verbose=True)
-
-
 query = """
 Vou viajar para Berlim em Novembro de 2024.
 Quero que faça um roteiro de viagem para mim com os os events que irão ocorrer na data da viagem e com o preço de passagem de São Paulo para Berlim.
 """
 
-agent_executor.invoke({"input": query})
+def reserchAgent(query, llm):
+    tools = load_tools(['ddg-search', 'wikipedia'], llm= llm)
+    prompt = hub.pull("hwchase17/react")
+    agent = create_react_agent(llm, tools, prompt)
+    agent_executor = AgentExecutor(agent=agent, tools=tools, prompt=prompt, verbose=True)
+    webContext = agent_executor.invoke({"input": query})
+    return webContext['output']
 
+print(reserchAgent(query, llm))
