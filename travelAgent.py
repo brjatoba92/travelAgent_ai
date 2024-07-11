43a93ff0a158492763341043afd313d27d5693f6
brjatoba92 <brjatoba92@gmail.com>
2024-07-11 00:20:06 -0300

    Agent Supervisor Implementation

--- travelAgent.py
+++ travelAgent.py
@@ -7,10 +7,11 @@ from langchain import hub
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_community.vectorstores import Chroma
 import bs4
+
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.prompts import PromptTemplate
 
-
-
+from langchain_core.runnables import RunnableSequence
 
 
 llm = ChatOpenAI(model="gpt-3.5-turbo")
@@ -21,7 +22,7 @@ Quero que faça um roteiro de viagem para mim com os os events que irão ocorrer
 """
 
 def reserchAgent(query, llm):
-    tools = load_tools(['ddg-search', 'wikipedia'], llm= llm)
+    tools = load_tools(["ddg-search", "wikipedia"], llm= llm)
     prompt = hub.pull("hwchase17/react")
     agent = create_react_agent(llm, tools, prompt)
     agent_executor = AgentExecutor(agent=agent, tools=tools, prompt=prompt, verbose=True)
@@ -33,9 +34,8 @@ def reserchAgent(query, llm):
 
 def loadData():
     loader = WebBaseLoader(
-        webpaths=("https://www.dicasdeviagem.com/alemanha/"),
-        bs_kwargs=dict(parse_only=bs4.SoapStrainer(class_=("postcontentwrap", "pagetitleloading background-imaged loading-dark")))
-    )
+    web_paths=("https://www.dicasdeviagem.com/alemanha/"),
+    bs_kwargs=dict(parse_only=bs4.SoapStrainer(class_=("postcontentwrap", "pagetitleloading background-imaged loading-dark"))))
     docs = loader.load()
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
     splits = text_splitter.split_documents(docs)
@@ -47,5 +47,35 @@ def loadData():
 def getRelevantDocs(query):
     retriever =loadData()
     relevant_documents = retriever.invoke(query)
+    print(relevant_documents)
     return relevant_documents
 
+
+def supervisorAgent(query, llm, webContext, relevant_documents):
+    prompt_template = """
+    Você é um gerente de uma agencia de viagens. Sua resposta final deverá ser um roteiro de viagem completo e detalhado.
+    Uilize o contexto de eventos de preços de passagens, o input do usuario e também os documentos relevantes para elaborar o roteiro.
+    Contexto: {webContext}
+    Documento relevante: {relevant_documents}
+    Usuário: {query}
+    Assistente: 
+    """
+
+    prompt = PromptTemplate(
+        input_variables = ['webContext', 'relevant_documents', 'query'],
+        template = prompt_template
+    )
+
+    sequence = RunnableSequence(prompt | llm)
+    response = sequence.invoke({"webContext": webContext, "relevant_documents": relevant_documents, "query": query})
+    return response
+
+
+def getResponse(query, llm):
+    webContext = reserchAgent(query, llm)
+    relevant_documents = getRelevantDocs(query)
+    response = supervisorAgent(query,llm, webContext, relevant_documents)
+    return response
+
+
+print(getResponse(query, llm).content)
